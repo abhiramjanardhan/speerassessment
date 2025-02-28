e73954a0c5c81fd6ba349aad5c0ee15fe33e5faf
Abhiram Janardhan <abhiramjanardhan@gmail.com>
2025-02-28 12:36:49 -0800

    Initial commit

---
+++ src/main/java/com/assessment/speernotes/SpeernotesApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.speernotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeernotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeernotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assessment/speernotes/SpeernotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.speernotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeernotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
