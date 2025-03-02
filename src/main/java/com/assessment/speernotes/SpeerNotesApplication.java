51c57b3a88bab491f8c0fbc59c2026e4430434c5
Abhiram Janardhan <abhiramjanardhan@gmail.com>
2025-03-01 23:36:12 -0800

    Configuring environment values

--- src/main/java/com/assessment/speernotes/SpeerNotesApplication.java
+++ src/main/java/com/assessment/speernotes/SpeerNotesApplication.java
@@ -1,5 +1,6 @@
 package com.assessment.speernotes;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpeerNotesApplication {
 
 	public static void main(String[] args) {
+		// Load .env file only if the environment variable is NOT already set
+		Dotenv dotenv = Dotenv.configure().ignoreIfMalformed().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(SpeerNotesApplication.class, args);
 	}
 
